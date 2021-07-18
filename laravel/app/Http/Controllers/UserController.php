0023b49fcf13dd36cdfdafd607148c4e59624ae7
Ana Claudia Nogueira <anaclaudia@mayacosmeticos.com.br>
2021-07-18 18:50:13 -0300

    Adding User Request Validation

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Requests\UserRequest;
 
 class UserController extends Controller
 {
@@ -31,9 +32,12 @@ class UserController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UserRequest $request)
     {
-        //
+        
+        $flight->name = $request->name;
+
+        $flight->save();
     }
 
     /**
---
+++ laravel/app/Http/Requests/UserRequest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [            
+            'name' => 'required',
+            'email' => [
+                'required',
+                'email',
+                Rule::unique('users')->ignore($this->user),
+            ],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'required' => 'O campo é obrigatório.',
+            'email' => 'O e-mail deve ser um endereço de e-mail válido.',
+            'unique' => 'O campo requer um valor único.',
+        ];
+    }
+
+     /**
+     * Transform the error messages into JSON
+     *
+     * @param array $errors
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json($validator->errors(), 422));
+    }
+}
