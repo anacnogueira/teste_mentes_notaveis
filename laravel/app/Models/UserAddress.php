36ea0068b38b778e92d4f35f75a516bc1eab698e
Ana Claudia Nogueira <anaclaudia@mayacosmeticos.com.br>
2021-07-18 19:13:26 -0300

    Create UserAddress Model and Controller

---
+++ laravel/app/Models/UserAddress.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserAddress extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'address',
+        'state_id',
+        'city_id'
+    ];
+}
