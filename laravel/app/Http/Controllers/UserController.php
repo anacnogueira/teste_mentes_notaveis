e5b77da902e74424c0f8565ba8f935f36d358bd1
Ana Claudia Nogueira <anaclaudia@mayacosmeticos.com.br>
2021-07-18 19:07:27 -0300

    Delete  User

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -75,8 +75,10 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(User $user)
     {
-        //
+        $user->delete();
+
+        return response()->json($user);
     }
 }
